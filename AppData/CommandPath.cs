df9e4e50dbb990424ca2fa4655bde223cfa4b054
Howard Kapustein <howardk@microsoft.com>
2023-04-29 00:19:33 -0700

    Added -c/--localcache

--- AppData/CommandPath.cs
+++ AppData/CommandPath.cs
@@ -9,6 +9,7 @@ namespace AppData
 @"USAGE: AppData Path <packagefamilyname> [options] - Retrieves path(s)
 options:
   -a, --all         = All application data stores [default]
+  -c, --localcache  = LocalCache application data store
   -l, --local       = Local application data store
   -r, --roaming     = Roaming application data store
   -t, --temporary   = Temporary application data store
@@ -28,7 +29,8 @@ appdata path contosso.games.solitaire_1234567890abc
             All = -1,
             Local = Windows.Storage.ApplicationDataLocality.Local,
             Roaming = Windows.Storage.ApplicationDataLocality.Roaming,
-            Temporary = Windows.Storage.ApplicationDataLocality.Temporary
+            Temporary = Windows.Storage.ApplicationDataLocality.Temporary,
+            LocalCache = Windows.Storage.ApplicationDataLocality.LocalCache,
         };
         Locality locality = Locality.All;
 
@@ -48,6 +50,8 @@ appdata path contosso.games.solitaire_1234567890abc
         {
             if (arg.Equals("-a", StringComparison.InvariantCultureIgnoreCase) || arg.Equals("--all", StringComparison.InvariantCultureIgnoreCase))
                 locality = Locality.All;
+            else if (arg.Equals("-c", StringComparison.InvariantCultureIgnoreCase) || arg.Equals("--localcache", StringComparison.InvariantCultureIgnoreCase))
+                locality = Locality.LocalCache;
             else if (arg.Equals("-l", StringComparison.InvariantCultureIgnoreCase) || arg.Equals("--local", StringComparison.InvariantCultureIgnoreCase))
                 locality = Locality.Local;
             else if (arg.Equals("-r", StringComparison.InvariantCultureIgnoreCase) || arg.Equals("--roaming", StringComparison.InvariantCultureIgnoreCase))
@@ -58,6 +62,8 @@ appdata path contosso.games.solitaire_1234567890abc
                 base.Parse(arg);
         }
 
+        protected override int GetRequiredArgumentsCount() { return 1; }
+
         public override void Execute()
         {
             PrintLineVerbose("open");
@@ -65,11 +71,13 @@ appdata path contosso.games.solitaire_1234567890abc
 
             PrintLineVerbose("execute");
             if (this.locality == Locality.All || this.locality == Locality.Local)
-                PrintLineFormat("    Local: {0}", appdata.LocalFolder.Path);
+                PrintLineFormat("     Local: {0}", appdata.LocalFolder.Path);
+            if (this.locality == Locality.All || this.locality == Locality.LocalCache)
+                PrintLineFormat("LocalCache: {0}", appdata.LocalCacheFolder.Path);
             if (this.locality == Locality.All || this.locality == Locality.Roaming)
-                PrintLineFormat("  Roaming: {0}", appdata.RoamingFolder.Path);
+                PrintLineFormat("   Roaming: {0}", appdata.RoamingFolder.Path);
             if (this.locality == Locality.All || this.locality == Locality.Temporary)
-                PrintLineFormat("Temporary: {0}", appdata.TemporaryFolder.Path);
+                PrintLineFormat(" Temporary: {0}", appdata.TemporaryFolder.Path);
         }
     }
 }
